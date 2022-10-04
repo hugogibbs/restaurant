8dc65d9f07e63c520e760cc62a28e0564825999e
Hugo Gibbs <hugo.d.gibbs@gmail.com>
2022-10-04 13:26:40 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
+// import all the modules of the website
+
 import {homePageLoad} from './website.js';
 import {menuLoad} from './menu.js';
 import {contactLoad} from './contact.js';
-import './style.css';
+
+//import the CSS file
+
+import './style.css';   
+
+//import the images
+
+import '../dist/burger.png';
+import '..dist/cheeseburger.png';
+import '../dist/doubleCheeseburger.png';
+import '../dist/classic.png';
+import '../dist/deluxe.png';
+import '../dist/location.png';
+import '../dist/map.png';
 
 homePageLoad();
 
